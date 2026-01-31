7f192605735d86fb6512bbf9de3cf18c25aacfff
Helm <none>
2026-01-31 22:42:16 +0800

    police check play sound

---
+++ resource/custom/sound_answer/sound_answer_res.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name SoundAnswerRes
+
+
+@export var question: String
+@export var answer: PackedInt32Array
--- system/events.gd
+++ system/events.gd
@@ -3,4 +3,6 @@ extends Node
 signal request_change_level(level_name: StringName)
 
 signal request_play_music(music_name: StringName)
+signal request_stop_music
+signal request_replay_music
 signal request_play_sound(sound_name: StringName)
