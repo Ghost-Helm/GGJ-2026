2b69431451883490ffc9ca0297271f4dc5a02acd
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2026-02-01 11:13:23 +0800

    眼睛加上边框

--- level/mask_face/fall_mask.gd
+++ level/mask_face/fall_mask.gd
@@ -2,7 +2,9 @@ extends Control
 class_name FallMask
 
 @onready var texture_rect: TextureRect = $TextureRect
+@onready var eye_frame: TextureRect = $EyeFrame
 
 func setup(res:FaceRes):
-    texture_rect.texture = res.face_img
-    
+	texture_rect.texture = res.face_img
+	eye_frame.visible = res.type == FaceRes.FACE_TYPE.Eye
+	
