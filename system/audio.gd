44c7f6b6ecfc1afdab289fea06f7c7084a0624ad
Helm <none>
2026-02-01 09:15:55 +0800

    stop and replay music when play sound

--- system/audio.gd
+++ system/audio.gd
@@ -1,6 +1,14 @@
 extends Node
 
 
+func _stop_music() -> void:
+    music_player.stop()
+
+
+func _replay_music() -> void:
+    music_player.play()
+
+
 func _play_music(music_name) -> void:
     music_player.stream = _music_res_map[music_name]
     music_player.play()
@@ -14,6 +22,8 @@ func _play_sound(sound_name: StringName) -> void:
 func _ready() -> void:
     Events.connect("request_play_music", _play_music)
     Events.connect("request_play_sound", _play_sound)
+    Events.connect("request_stop_music", _stop_music)
+    Events.connect("request_replay_music", _replay_music)
 
     for music_res in music_res_group.load_all():
         _music_res_map[music_res.music_name] = music_res.stream
