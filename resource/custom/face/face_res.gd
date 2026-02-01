52a15a8a29a4b773ed27f482cf76227c35761527
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2026-02-01 13:22:59 +0800

    五官的表情

--- resource/custom/face/face_res.gd
+++ resource/custom/face/face_res.gd
@@ -2,8 +2,10 @@ extends Resource
 class_name FaceRes
 
 enum FACE_TYPE{Eye, Eyebrow, Nose, Mouse, Other}
+enum EMOTION_TYPE{Happy, Sad, Nothing}
 
 @export var face_name: StringName
 @export var face_img: Texture2D
 @export var type: FACE_TYPE
 @export var dialog: String
+@export var emotion:EMOTION_TYPE
